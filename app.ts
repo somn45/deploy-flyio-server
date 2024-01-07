0ee742bf8aa5282da403b8834da982b5cd0727d8
somn45 <somnsomn45@gmail.com>
2024-01-07 21:24:25 +0900

    hello 경로 추가

--- app.ts
+++ app.ts
@@ -3,15 +3,20 @@ import fetch from 'node-fetch';
 import cors from 'cors';
 
 const app = experss();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
 app.get('/', async (req, res) => {
+  console.log('hello flyio');
   const response = await (
     await fetch('https://yts.mx/api/v2/list_movies.json')
   ).json();
   return res.json({ movies: response.data.movies });
+});
+
+app.get('/hello', (req, res) => {
+  return res.send('Hello Fly.io Server');
 });
 
 app.listen(PORT, () => `✅ ${PORT} port server connected success`);
